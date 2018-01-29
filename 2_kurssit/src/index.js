7b8bb5d40b83cc49e32d7ac29024e98902680ae4
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-01-29 15:10:35 +0200

    2_1

---
+++ 2_kurssit/src/index.js
@@ -0,0 +1,68 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Osa = (props) => <p>{props.osa} {props.tehtavia}</p>
+
+const Otsikko = (props) => <h1>{props.kurssi.nimi}</h1>
+
+const Sisalto = (props) => {
+    
+    return (
+        <div>
+            {props.kurssi.osat.map(osa => <Osa key={osa.id} osa={osa.nimi} tehtavia={osa.tehtavia} />)}
+        </div>
+    )
+}
+
+const Yhteensa = (props) => {
+    /*const [osa1, osa2, osa3] = props.kurssi.osat
+
+    return (
+        <p>yhteensä {osa1.tehtavia + osa2.tehtavia + osa3.tehtavia} tehtävää</p>
+    )*/
+    return (<p></p>)
+}
+
+const Kurssi = ({ kurssi }) => {
+    return (
+        <div>
+            <Otsikko kurssi={kurssi} />
+            <Sisalto kurssi={kurssi} />
+            <Yhteensa kurssi={kurssi} />
+        </div>
+    )
+}
+
+const App = () => {
+    const kurssi = {
+        nimi: 'Half Stack -sovelluskehitys',
+        osat: [
+            {
+                nimi: 'Reactin perusteet',
+                tehtavia: 10,
+                id: 1
+            },
+            {
+                nimi: 'Tiedonvälitys propseilla',
+                tehtavia: 7,
+                id: 2
+            },
+            {
+                nimi: 'Komponenttien tila',
+                tehtavia: 14,
+                id: 3
+            }
+        ]
+    }
+
+    return (
+        <div>
+            <Kurssi kurssi={kurssi} />
+        </div>
+    )
+}
+
+ReactDOM.render(
+    <App />,
+    document.getElementById('root')
+)
