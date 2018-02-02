8140491ef75bd4fa908bef1b4902dffe2fd9e2ba
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-02-02 12:04:41 +0200

    2_13

--- 2_maat/src/App.js
+++ 2_maat/src/App.js
@@ -23,27 +23,27 @@ class App extends Component {
     this.setState({ search: event.target.value })
   }
 
+  onClickSelect = (name, event) => {
+    this.setState({ search: name })
+  }
+
   render() {
 
     const show = this.state.countries.filter(country => {
       return country.name.toLowerCase().includes(this.state.search.toLowerCase())
     })
 
-    if (this.state.countries.length > 0) {
-      console.log(this.state.countries[0].name)
-    }
-
     return (
       <div>
         <p>find countries</p>
         <input value={this.state.search} onChange={this.handleSearch} />
-        <Display show={show} />
+        <Display show={show} onClickSelect={this.onClickSelect}/>
       </div>
     );
   }
 }
 
-const Display = ({ show }) => {
+const Display = ({ show, onClickSelect }) => {
   if (show.length > 0) {
     if (show.length > 10) {
       return <p>too many results</p>
@@ -52,7 +52,7 @@ const Display = ({ show }) => {
     } else {
       return (
         <div>
-          {show.map(country => <p key={country.alpha2Code}>{country.name}</p>)}
+          {show.map(country => <p key={country.alpha2Code} onClick={(e) => onClickSelect(country.name, e)}>{country.name}</p>)}
         </div>
       )
     }
@@ -67,7 +67,7 @@ const Country = ({ data }) => {
       <h1>{data.name} {data.nativeName}</h1>
       <p>capital: {data.capital}</p>
       <p>population: {data.population}</p>
-      <img src={data.flag} width="30%" height="30%"/>
+      <img src={data.flag} alt='flag' width="30%" height="30%"/>
     </div>
   )
 }
