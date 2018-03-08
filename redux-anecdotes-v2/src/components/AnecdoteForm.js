d3e3347ff24732bd59397c2293abd42baaceecc5
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-03-08 15:31:14 +0200

    6.11

--- redux-anecdotes-v2/src/components/AnecdoteForm.js
+++ redux-anecdotes-v2/src/components/AnecdoteForm.js
@@ -1,21 +1,22 @@
 import React from 'react'
 import { anecdoteCreation } from './../reducers/anecdoteReducer'
-import { notifyCreated, clearNotification } from '../reducers/notificationReducer';
+import { notifyCreated, clearNotification } from '../reducers/notificationReducer'
+import anecdoteService from '../services/anecdotes'
 import { connect } from 'react-redux'
 
 class AnecdoteForm extends React.Component {
-  handleSubmit = (e) => {
+  handleSubmit = async (e) => {
     e.preventDefault()
     const content = e.target.anecdote.value
-    this.props.anecdoteCreation(content)
-    this.props.notifyCreated(content)
-
+    e.target.anecdote.value = ''
+    const newAnecdote = await anecdoteService.create(content)
+    this.props.anecdoteCreation(newAnecdote)
+    this.props.notifyCreated(newAnecdote.content)
     setTimeout(() => {
       this.props.clearNotification()
     }, 5000)
-
-    e.target.anecdote.value = ''
   }
+  
   render() {
     return (
       <div>
