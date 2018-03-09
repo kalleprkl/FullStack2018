bb5ab13558b690c415a73712a9f918b67f8938ec
Kalle Perkoila <kalle.perkoila@helsinki.fi>
2018-03-09 13:30:06 +0200

    6.17

--- routed-anecdotes/src/App.js
+++ routed-anecdotes/src/App.js
@@ -13,10 +13,25 @@ const AnecdoteList = ({ anecdotes }) => (
   <div>
     <h2>Anecdotes</h2>
     <ul>
-      {anecdotes.map(anecdote => <li key={anecdote.id} >{anecdote.content}</li>)}
+      {anecdotes.map(anecdote =>
+        <li key={anecdote.id} >
+          <Link to={`/anecdotes/${anecdote.id}`} >{anecdote.content}</Link>
+        </li>
+      )}
     </ul>
+
   </div>
 )
+
+const Anecdote = ({ anecdote }) => {
+  return (
+    <div>
+      <h2>{`${anecdote.content} by ${anecdote.author}`}</h2>
+      <p>{`has ${anecdote.votes} votes`}</p>
+      <p>for more info see <a href={anecdote.info}>{anecdote.info}</a></p>
+    </div>
+  )
+}
 
 const About = () => (
   <div>
@@ -120,8 +135,9 @@ class App extends React.Component {
     this.setState({ anecdotes: this.state.anecdotes.concat(anecdote) })
   }
 
-  anecdoteById = (id) =>
-    this.state.anecdotes.find(a => a.id === id)
+  anecdoteById = (id) => {
+    return this.state.anecdotes.find(a => a.id === id)
+  }
 
   vote = (id) => {
     const anecdote = this.anecdoteById(id)
@@ -146,6 +162,9 @@ class App extends React.Component {
             <Route exact path='/' render={() => <AnecdoteList anecdotes={this.state.anecdotes} />} />
             <Route path='/create' render={() => <CreateNew addNew={this.addNew} />} />
             <Route path='/about' render={() => <About />} />
+            <Route exact path='/anecdotes/:id' render={({ match }) =>
+              <Anecdote anecdote={this.anecdoteById(match.params.id)} />}
+            />
           </div>
         </Router>
         <div>
